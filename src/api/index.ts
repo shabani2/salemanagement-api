92608c6152e0308c630aabcab2bba5494e032ca5
shabani2 <shabanibernard1@gmail.com>
2025-04-29 15:26:10 +0100

    vercel deployment

--- src/api/index.ts
+++
@@ -1,4 +0,0 @@
-import app from "./../index"
-import { default as serverlessExpress } from "@vendia/serverless-express";
-
-export default serverlessExpress({ app });
---
+++ src/app/index.ts
@@ -0,0 +1,8 @@
+import { Request, ParamsDictionary, Response } from "express-serve-static-core";
+import { IncomingMessage, ServerResponse } from "http";
+import { ParsedQs } from "qs";
+import app from "../index";
+
+export default function handler(req: IncomingMessage | Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number> | ServerResponse<IncomingMessage>) {
+  return app(req, res);
+}
