70466ffb4690b59f5cb0a8fbb4fc721509acac54
sagadi123 <bernard@243technologies.com>
2025-08-09 13:13:48 +0100

    setting up du middleware upload

--- src/Middlewares/upload.ts
+++ src/Middlewares/upload.ts
@@ -3,19 +3,27 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const role = (req.body as { role?: string }).role || "default";
-    const uploadDir = path.join(__dirname, `../../assets/${role}`);
+let storage: multer.StorageEngine;
 
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+if (process.env.NODE_ENV === "production") {
+  // En prod : stockage en mémoire pour envoyer vers GCS
+  storage = multer.memoryStorage();
+} else {
+  // En local : stockage sur disque avec dossier par rôle
+  storage = multer.diskStorage({
+    destination: (req: Request, file, cb) => {
+      const role = (req.body as { role?: string }).role || "default";
+      const uploadDir = path.join(__dirname, `../../assets/${role}`);
+
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+      cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+}
 
 export const upload = multer({ storage });
